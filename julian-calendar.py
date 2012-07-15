d6711982c9aa35cb2e5a089d9f27ab60be47f6ba
1rick <ineedrick@gmail.com>
2012-07-15 21:55:20 +0900

    a few adjustments after learning some new things.
    Still just taking one date. Goal is to take two dates from user and
    turn into google search string.

--- julian-calendar.py
+++ julian-calendar.py
@@ -3,15 +3,23 @@
 Following the formula given here: http://quasar.as.utexas.edu/BillInfo/JulianDatesG.html.
 Next to do is turn it into a Google date range search."""
 
-y = input("Type the year: ")
-m = input("Type the month: ")
-d = input("Type the numeric day of the month: ")
+from time import gmtime, strftime
 
-a = int(y)/100
-b = a/4
-c = 2-a+b
-e = 365.25*(int(y)+4716)
-f = 30.6001*(int(m)+1)
-juliandate = c+int(d)+e+f-1524.5
+def julianify(date):
+    """given a date 'n' in 8-digit format, parse and convert to julian"""
+    d = n[0:2]
+    m = n[2:4]
+    Y = n[4:8]
+    a = int(Y)/100
+    b = a/4
+    c = 2-a+b
+    e = 365.25*(int(Y)+4716)
+    f = 30.6001*(int(m)+1)
+    return (c+int(d)+e+f-1524.5)
 
-print("The Julian Calendar date is", juliandate)
+while True:
+    n = input("Type the date (in DDMMYYYY format) or enter for today: ")
+    if not n:
+        n = strftime("%d%m%Y")
+    print("The Julian Calendar date is", julianify(n))
+    break
