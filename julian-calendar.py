23723c5bc7e254d428dc55c5209b283a34cb9901
1rick <ineedrick@gmail.com>
2012-07-20 00:05:12 +0900

    updated for two date inputs

--- julian-calendar.py
+++ julian-calendar.py
@@ -5,7 +5,18 @@ Next to do is turn it into a Google date range search."""
 
 from time import gmtime, strftime
 
-def julianify(date):
+searchterm = input("type search term: ")
+
+d1 = input("type date 1: ")
+if not d1:
+	d1 = strftime("%d%m%Y")
+
+
+d2 = input("type date 2: ")
+if not d2:
+    d2 = strftime("%d%m%Y")
+
+def julianify(n):
     """given a date 'n' in 8-digit format, parse and convert to julian"""
     d = n[0:2]
     m = n[2:4]
@@ -17,9 +28,4 @@ def julianify(date):
     f = 30.6001*(int(m)+1)
     return (c+int(d)+e+f-1524.5)
 
-while True:
-    n = input("Type the date (in DDMMYYYY format) or enter for today: ")
-    if not n:
-        n = strftime("%d%m%Y")
-    print("The Julian Calendar date is", julianify(n))
-    break
+print(searchterm+"daterange:"+str(julianify(d1))+"-"+str(julianify(d2)))
--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 #Convert date to Julian Calendar
 
-Just learning python, so hoping to turn this into a web tool for taking two gregorian calendar inputs and outputting the converted julian calendar dates into a Google date range search string, of the following format:
+Just learning python, so hoping to turn this into a web tool for taking a search term and two gregorian calendar inputs and outputting the converted julian calendar dates into a Google date range search string, of the following format:
 
 `barack obama daterange:2454102-2454133`
+(for searchterm = 'barack obama')
 
 To my knowledge, there's no range search tool out there, and I'm getting tired of converting to Julian and then bringing that to Google.
 
